0e18c64afd0806896cd3eac6ba881f44969a2789
ERICK-GABRIEL <erick3000msn9@gmail.com>
2022-04-28 18:44:19 -0300

    Aulas

---
+++ Incremento e Decremento/src/br/com/entra21/encremento/decremento/Main.java
@@ -0,0 +1,21 @@
+package br.com.entra21.encremento.decremento;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		float idade=55;
+	
+		
+		idade= idade * 2;
+		
+		
+		
+		
+		
+
+	}
+
+}
